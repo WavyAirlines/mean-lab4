5ea61019b65492d4ad2fc0e9e173cdd4190185f0
WavyAirlines <113380353+WavyAirlines@users.noreply.github.com>
2024-02-05 16:13:58 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+
+let index = require('./controllers/index');
+let users = require('./controllers/users');
+
+// custom controllers
+let posts = require('./controllers/posts');
+
+// custom imports
+let mongoose = require('mongoose');
+let dotenv = require('dotenv');
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// if app is not in production mode, connect to .env file for global vars
+// these global vars set in the Render dashboard in production
+if (process.env.NODE_ENV != 'production') {
+  require('dotenv').config();
+}
+
+// mongodb connection BEFORE controllers included
+mongoose.connect(process.env.CONNECTION_STRING)
+.then((res) => { console.log('Connected to MongoDB') })
+.catch(() => { console.log('MongoDB connection failed') });
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/posts', posts);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ controllers/posts.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let router = express.Router();
+
+// Post model for CRUD
+let Post = require('../models/post');
+
+/* GET: /posts => show main blog page */
+router.get('/', async(req, res) => {
+    // use model to fetch all Post data from MongoDB
+    let posts = await Post.find();
+    // console.log(posts[0]);
+
+    // load view and pass all the json data to it for display
+    res.render('posts/index', {
+        title: 'Our Lastest Random Thoughts',
+        posts: posts
+    });
+});
+router.get('/posts/:_id', async(req,res) => {
+let post = Post.findById(req.params._id);
+})
+
+// make public
+module.exports = router;
